3fc7107b3111912fe4ead2a053b39cec30ff39f9
Rogério Morais <rogeriofsantos@gmail.com>
2022-02-04 00:01:35 -0300

    ci teste

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,7 +3,6 @@
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 cp .env.example.env .env
 cp .env.testing.example .env.testing
-chown -R www-data:www-data .
 composer install
 php artisan key:generate
 php artisan migrate
